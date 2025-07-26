44d69c846fbd37e105562abc8ce249275ff43204
Arpan-Roy-1993 <arpan.roy93@gmail.com>
2025-07-26 12:00:05 -0700

    triggering my workflows 3

--- test_eval.py
+++ test_eval.py
@@ -1,3 +1,3 @@
-user_input = input("Enter math expression: ")
-result = eval(user_input)  # INSECURE
-print("Result:", result)
+# user_input = input("Enter math expression: ")
+# result = eval(user_input)  # INSECURE
+# print("Result:", result)
